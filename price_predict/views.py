6f1573d7eea6bae2f5bd7d588b625e7628a92805
Aryan Khokhar <aryankhokhar2000@gmail.com>
2023-10-25 22:55:27 +0530

    final

--- price_predict/views.py
+++ price_predict/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from joblib import load
+import locale
+
+
+model = load('./savedModels/models.joblib')
+locale.setlocale(locale.LC_MONETARY, 'en_IN')
+'en_IN'
 from .models import Area_data
 def home(request):
     return render(request, 'price_prediction.html')
@@ -7,4 +14,9 @@ def price_predictions(request):
     if(request.method == 'POST'):
         area = request.POST['area']
     Area_data(area = area).save()
-    return render(request, 'price_prediction.html')
+    print("Area == ",area)
+    predicted_price = model.predict([[int(area)]])
+    print("Predicted Price =",predicted_price)
+    price = locale.currency(int(predicted_price), grouping=True) 
+    print("Price =",price)
+    return render(request, 'price_prediction.html',{'price': price})
